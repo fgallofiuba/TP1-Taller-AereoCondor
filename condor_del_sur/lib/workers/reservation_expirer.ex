f103bbb97da6def804fdb9f1ed20cc6f5d6d498c
Felipe Gallo Terrera <fgallo@fi.uba.ar>
2026-04-24 19:15:36 -0300

    Implemented full function in the reservation_epxired

---
+++ condor_del_sur/lib/workers/reservation_expirer.ex
@@ -0,0 +1,15 @@
+defmodule CondorDelSur.Workers.ReservationExpirer do
+  @moduledoc """
+  Worker efímero que expira una reserva si sigue `:pending` después
+  de un tiempo dado.
+  """
+
+  @default_timeout_ms 30_000
+
+  def start(flight_server, reservation_id, timeout_ms \\ @default_timeout_ms) do
+    spawn(fn ->
+      Process.sleep(timeout_ms)
+      send(flight_server, {:expire_if_pending, reservation_id})
+    end)
+  end
+end
